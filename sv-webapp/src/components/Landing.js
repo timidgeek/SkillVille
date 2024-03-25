b5addf4561ebfb0c4f91d80684dddbc0508f35a2
timidgeek <9liveslinds@gmail.com>
2024-03-25 17:47:55 -0500

    landing page looking adorable, working on header now

--- sv-webapp/src/components/Landing.js
+++ sv-webapp/src/components/Landing.js
@@ -4,12 +4,24 @@ import React from "react";
 import '../styles/Landing.css';
 import logo from '../assets/images/sv-logoTM.png';
 import cloud from '../assets/game_pngs/Cloud.png'
+import lag from '../assets/images/sv-lag.png';
+import play from '../assets/game_pngs/navigation/pink_play.png';
+import pb from '../assets/game_pngs/navigation/pink_brown.png';
+import yellow from '../assets/game_pngs/navigation/btn_play.png';
 
 export default function Landing() {
   return (
     <div className="Landing">
-      <img src={logo} className="Landing-logo" alt="SkillVille" />
-      <p className="Learn">Learn and Grow</p>
+      <div className="Cloud-background"></div> 
+      <div className="Content">
+        <img src={logo} className="Landing-logo" alt="SkillVille" />
+        <p className="Learn">Learn and Grow</p>
+      </div>
+      <div className="Content">
+        <a href="https://github.com/teylorchapman" target="_blank" rel="noreferrer">
+          <button className="Play-btn" alt="Play-button">PLAY</button>
+        </a>
+      </div>
     </div>
   )
 }
