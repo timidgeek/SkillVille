72a1ae491356921d091445d74dac60238d487731
timidgeek <9liveslinds@gmail.com>
2024-03-25 20:30:31 -0500

    navigation working well, styling header more

--- sv-webapp/src/App.js
+++ sv-webapp/src/App.js
@@ -1,17 +1,21 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Landing from './components/Landing';
+import Credits from './components/Credits';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Landing />
-      <header className="App-header">
-       
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+          <Route path="/credits" element={<Credits />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
